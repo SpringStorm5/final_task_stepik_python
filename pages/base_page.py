d5ed30ddd5e8613098f586ab8013efaf6f96fdbd
Galina Balashkova <springstorm666@gmail.com>
2019-07-24 09:59:26 +0300

    learning Page Object. Add classes for locators and ations

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,16 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage(object):
-	def __init__(self, browser, url):
+	def __init__(self, browser, url, timeout=10):
 		self.browser = browser
 		self.url = url
+		self.browser.implicitly_wait(timeout)
 	def open(self):
-		browser.get(self)
+		self.browser.get(self.url)
+	def is_element_present(self, how, what):
+		try:
+			self.browser.find_element(how, what)
+		except NoSuchElementException:
+			raise False
+		finally:
+			True
+		
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,1 +1,9 @@
-Режим вывода команд на экран (ECHO) включен.
+from selenium.webdriver.common.by import By
+from .base_page import BasePage 
+class MainPage(BasePage):
+	def go_to_login_page(self):
+		self.browser.find_element(By.CSS_SELECTOR, "a#login_link")
+
+	def should_be_login_link(self):
+		assert self.is_element_present(By.CSS_SELECTOR, "a#login_link"), "Login link is not presented"
+    	
