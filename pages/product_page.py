97b3e998eb665da4baf4e51995f15d4a3a8dd741
Galina Balashkova <springstorm666@gmail.com>
2019-07-31 23:13:26 +0300

    edit messages errors

--- pages/product_page.py
+++ pages/product_page.py
@@ -15,16 +15,12 @@ class ProductPage(BasePage):
     def should_be_product_add_basket(self):
         productname = self.browser.find_element(*ProductPageLocators.NAME_BOOK).text
         productname_in_busket = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE_PRODUCT_NAME).text
-        print(productname)
-        print(productname_in_busket)
-        assert productname == productname_in_busket, "test message"
+        assert productname == productname_in_busket, "имя товара не совпадает с товаром в корзине"
 
     def should_priceproduct_equal_basket(self):
         price_basket = self.browser.find_element(*ProductPageLocators.INFO_MESSAGE_CART_PRICE).text
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        print(price_basket)
-        print(product_price)
-        assert price_basket == product_price, "test two message"
+        assert price_basket == product_price, "цена товара не совпадает с ценой в корзине"
 
     def check_adding_message_not_present(self):
         assert self.is_not_element_present(*ProductPageLocators.INFO_MESSAGE_PRODUCT_NAME), "Product adding message must be absent"
